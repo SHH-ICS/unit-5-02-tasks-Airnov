3fdb18acd26fcf1610c405205c38bd3c27e89aa6
Airnov <124378651+Airnov@users.noreply.github.com>
2023-05-08 14:09:11 +0000

    comit

--- Q2Reverse.py
+++ Q2Reverse.py
@@ -1,1 +1,7 @@
 # Create a program that will accept a word and output the word one letter at a time in reverse.
+
+string = input("Enter a word to see how it looks like in reverse: ")
+reversed_string = string[::-1]
+for letter in reversed_string:
+    print(letter)
+
